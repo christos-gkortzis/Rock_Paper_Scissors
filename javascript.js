722b853957c88ea88fcbdc274e641be1bbba1ccd
Christos Gkortzis <chrisgkortzis@outlook.com>
2023-07-23 19:24:52 +0300

    Create the getComputerChoice function that returns rock, paper, scissors randomly

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+
+function getComputerChoice(){
+    let x = Math.floor(Math.random() * (4 - 1) + 1);
+    if (x == 1){
+        return "ROCK";
+    } else if (x == 2){
+        return "PAPER"
+    } else {
+        return "SCISSORS"
+    }
+}
+
+ let choice = getComputerChoice();
+ console.log(choice);
+
+
