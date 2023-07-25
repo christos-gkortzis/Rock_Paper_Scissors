9a1598c1d302fe9f798ce2dfdceb92ddf62105c9
Christos Gkortzis <chrisgkortzis@outlook.com>
2023-07-25 12:29:39 +0300

    Create the playRound function that plays a single round with the computer

--- javascript.js
+++ javascript.js
@@ -10,7 +10,40 @@ function getComputerChoice(){
     }
 }
 
- let choice = getComputerChoice();
- console.log(choice);
+ 
+ function playRound( playerSelection, computerSelection){
+    let player = playerSelection.toUpperCase();
+    console.log(player)
+    if (player == "ROCK"){
+        if (computerSelection == "PAPER"){
+            return "You lose! Paper beats rock"
+        } else if (computerSelection == "SCISSORS"){
+            return "You win rock beats scissors!"
+        } else {
+            return "It's a tie!"
+        }
+    } else if (player == PAPER){
+        if (computerSelection == "ROCK"){
+            return "You win! Paper beats rock"
+        } else if (computerSelection == "SCISSORS"){
+            return "You lose scissors beats paper!"
+        } else {
+            return "It's a tie!"
+        }
+    } else {
+        if (computerSelection == "SCISSORS"){
+            return "It's a tie!"
+        } else if (computerSelection == "ROCK"){
+            return "You lose rock beats scissors!"
+        } else {
+            return "You win! Scissors beats papper"
+        }
+    }
+
+ }
+ const playerSelection = "rock";
+ const computerSelection = getComputerChoice();
+ console.log(computerSelection)
+ console.log(playRound(playerSelection, computerSelection));
 
 
